34129435e5a13a279e5b7726486564bc760736ea
Fluffy <mrsargent00@gmail.com>
2023-09-19 17:03:12 -0400

    added to HTML, restyled CSS and added to JS

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,89 +1,222 @@
-//questions
-const questions = [{
-    question: "Commonly used data types DO not include:",
-        answers:[
-            { text: "strings", correct: false },
-            { text: "booleans", correct: false },
-            { text: "alerts", correct: true },
-            { text: "numbers", correct: false },
-        ]
-},
-{
-    question: "Arrays in JavaScript can be used to store.",
-    answers: [
-            { text: "numbers and strings", correct: false},
-            { text: "other arrays", correct: false },
-            { text: "booleans", correct: false },
-            { text: "all the above", correct: true },
-        ]
-},
-{
-    question: "The condition in an if/else statement is enclosed with ___.",
-    answers: [
-            { text: "quotes", correct: true},
-            { text: "curly brackets", correct: false },
-            { text: "parenthesis", correct: false },
-            { text: "square brackets", correct: false },
-         ]
-},
-{
-    question: "String vaules must be enclosed with ___ when being assigned to variables.",
-        answers: [
-            { text: "commas", correct: false },
-            { text: "curly brackets", correct: false },
-            { text: "quotes", correct: true },
-            { text: "parenthesis", correct: false },
-        ]
-    }
-];
-
-const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("next-btn");
-
-let currentQuestionIndex = 0
-let score = 0
-
-function startQuiz(){
+// Array of questions and their answers
+const questions = [
+    {
+      question: "Commonly used data types do NOT include:",
+      answers: [
+        { text: "Strings", correct: false },
+        { text: "Booleans", correct: false },
+        { text: "Alerts", correct: true },
+        { text: "Numbers", correct: false },
+      ],
+    },
+    {
+      question: "The condition is in if / else statement is enclosed within:",
+      answers: [
+        { text: "Quotes", correct: false },
+        { text: "Curley brackets", correct: true },
+        { text: "Paretheses", correct: false },
+        { text: "Square brackets", correct: false },
+      ],
+    },
+    {
+      question: "Arrays in Javascript can be used to store:",
+      answers: [
+        { text: "Numbers or strings", correct: false },
+        { text: "Other arrays", correct: false },
+        { text: "Booleans", correct: false },
+        { text: "All of the above", correct: true },
+      ],
+    },
+    {
+      question:
+        "String values must be enclosed within ____ when being assigned to variables.",
+      answers: [
+        { text: "Commas", correct: false },
+        { text: "Curly brackets", correct: false },
+        { text: "Quotes", correct: true },
+        { text: "Parentheses", correct: false },
+      ],
+    },
+    {
+      question:
+        "A very useful tool during development and debugging for printing content to the debugger is:",
+      answers: [
+        { text: "Javascript", correct: false },
+        { text: "Terminal/Bash", correct: false },
+        { text: "For loops", correct: false },
+        { text: "Console log", correct: true },
+      ],
+    },
+  ];
+  
+  // Selecting HTML elements for actions
+  const questionElement = document.getElementById("question");
+  const answerButtons = document.getElementById("answer-buttons");
+  const nextButton = document.getElementById("next-btn");
+  
+  // Variables to keep track of the quiz
+  let currentQuestionIndex = 0;
+  let score = 0;
+  
+  // Function to start the quiz
+  function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "Next";
     showQuestion();
-}
-
-function showQuestion(){
+  }
+  
+  // Function displaying the current question
+  function showQuestion() {
     resetState();
-    let currentQuestion = questions[currentQuestionIndex];
-    let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
-
-    currentQuestion.answers.forEach(answers => {
-        const button = document.createElement("button");
-        button.innerHTML = answers.text;
-        button.classList.add("btn");
-        answerButtons.appendChild(button);
-        if(answers.correct){
-            button.dataset.correct = answers.correct;
-        }
-        button.addEventListener("click", selectAnswer);
+    const currentQuestion = questions[currentQuestionIndex];
+    const questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = `${questionNo}. ${currentQuestion.question}`;
+  
+    // Creating answer buttons and setting up event listeners
+    currentQuestion.answers.forEach((answer) => {
+      const button = document.createElement("button");
+      button.innerText = answer.text;
+      button.classList.add("btn");
+      answerButtons.appendChild(button);
+      if (answer.correct) {
+        button.dataset.correct = answer.correct;
+      }
+      button.addEventListener("click", selectAnswer);
     });
-}
-function resetState(){
+  }
+  
+  // Function to reset the interface
+  function resetState() {
     nextButton.style.display = "none";
-    while(answerButtons.firstChild){
-        answerButtons.removeChild(answerButtons.firstChild);
+    while (answerButtons.firstChild) {
+      answerButtons.removeChild(answerButtons.firstChild);
     }
-}
-
-function selectAnswer(e){
-    const selectedBtn = e.target;
-    const isCorrect = selectedBtn.dataset.correct === "true";
-    if(isCorrect){
-         selectedBtn.classList.add("correct");
-    }else{
-        selectedBtn.classList.add("incorrect");
+  }
+  
+  // Function to handle answer selection
+  function selectAnswer(e) {
+    const selectedButton = e.target;
+    const isCorrect = selectedButton.dataset.correct === "true";
+  
+    if (isCorrect) {
+      selectedButton.classList.add("correct");
+      score++;
+    } else {
+      selectedButton.classList.add("incorrect");
+      // Deduct 10 seconds from the timer for incorrect answers
+      timeLeft -= 10;
     }
-}
-
-startQuiz();
-
+  
+    Array.from(answerButtons.children).forEach((button) => {
+      if (button.dataset.correct === "true") {
+        button.classList.add("correct");
+      }
+      button.disabled = true;
+    });
+  
+    nextButton.style.display = "block";
+  }
+  
+  // Function to display the final score and submit it to the scoreboard
+  function showScore() {
+    resetState();
+  
+    // Create input box for user initials and submit button
+    const initialsInput = document.createElement("input");
+    initialsInput.type = "text";
+    initialsInput.placeholder = "Enter your initials";
+  
+    const submitButton = document.createElement("button");
+    submitButton.innerText = "Submit";
+    submitButton.addEventListener("click", function () {
+      const initials = initialsInput.value;
+      if (initials) {
+        // Save score to local storage
+        const savedScores = JSON.parse(localStorage.getItem("scores")) || [];
+        savedScores.push({ initials, score });
+        localStorage.setItem("scores", JSON.stringify(savedScores));
+        // Hide input and submit button after submission
+        initialsInput.value = "";
+        initialsInput.style.display = "none";
+        submitButton.style.display = "none";
+  
+        questionElement.innerHTML = `Score submitted successfully!`;
+        updateScoreboard(savedScores);
+      }
+    });
+    // Display user's score
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!<br>`;
+    questionElement.appendChild(initialsInput);
+    questionElement.appendChild(submitButton);
+  
+    nextButton.innerHTML = "Play again?";
+    nextButton.style.display = "block";
+  }
+  
+  // Function to update the scoreboard
+  function updateScoreboard(scores) {
+    // Sort the scores array in descending order based on the score
+    scores.sort((a, b) => b.score - a.score);
+  
+    const scoreboardElement = document.createElement("div");
+    scoreboardElement.innerHTML = "<h2>Scoreboard</h2>";
+  
+    if (scores.length === 0) {
+      scoreboardElement.innerHTML += "No scores yet.";
+    } else {
+      for (const entry of scores) {
+        scoreboardElement.innerHTML += `<p>${entry.initials}: ${entry.score}</p>`;
+      }
+    }
+    // Update and display the new scoreboard
+    const oldScoreboard = document.getElementById("scoreboard");
+    if (oldScoreboard) {
+      oldScoreboard.parentNode.removeChild(oldScoreboard);
+    }
+    scoreboardElement.id = "scoreboard";
+    document.body.appendChild(scoreboardElement);
+  }
+  
+  // Function to handle the "Next" button
+  function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+      showQuestion();
+    } else {
+      showScore();
+    }
+  }
+  // Event listener for the "Next" button
+  nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
+      handleNextButton();
+    } else {
+      startQuiz();
+    }
+  });
+  // Start the quiz and update the scoreboard
+  startQuiz();
+  updateScoreboard(JSON.parse(localStorage.getItem("scores")) || []);
+  
+  // Get the timer element
+  const timerElement = document.getElementById("timer");
+  
+  // Set the initial time in seconds
+  let timeLeft = 100;
+  
+  const timerInterval = setInterval(() => {
+    // Display the remaining time
+    timerElement.textContent = timeLeft;
+  
+    // Decrease the time by 1 second
+    timeLeft--;
+  
+    // If the timer reaches 0, stops the user and directs them to input their initials
+    if (timeLeft < 0) {
+      clearInterval(timerInterval);
+      timerElement.textContent = "Time's up!";
+      timeLeft = 0;
+      showScore(); // Call the showScore function
+    }
+  }, 1000)
